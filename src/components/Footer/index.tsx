ba72367e5b9467b157b609ae7bedd4689e9bfcbf
matheuxito <matheus.souza28042001@gmail.com>
2023-11-07 22:52:05 -0300

    chore: update footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,20 +4,33 @@ import Link from '@mui/material/Link';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-100 py-4">
+    <footer className="w-full bg-white py-2 text-raisin font-semibold min-h-[12vh]">
       <p className="text-center font-mono my-2">
         Projects created for the computer graphics course at UNIFESP
       </p>
-      <hr className="hidden md:flex justify-center w-2/5 my-4 border-gray-700 sm:mx-auto" />
+      <hr className="hidden md:flex justify-center w-2/5 my-2 border-gray-700 sm:mx-auto" />
       <div className="flex justify-center items-center">
-        <div className="font-mono whitespace-nowrap">
-          Created with React <FaReact className="inline-block" />, Three.js{' '}
-          <TbBrandThreejs className="inline-block" /> and{' '}
+        <div className="font-mono whitespace-nowrap hidden sm:inline-block">
+          Created with React <FaReact className="inline-block animate-spin" />,
+          Three.js <TbBrandThreejs className="inline-block" /> and{' '}
           <div className="inline-block">❤️</div> by{' '}
           <Link
             href="https://www.github.com/matheuxito"
             underline="hover"
             color="inherit"
+            target="_blank"
+          >
+            Matheuxito
+          </Link>{' '}
+          <FaGithub className="inline-block" />
+        </div>
+        <div className="font-mono whitespace-nowrap inline-block sm:hidden">
+          by{' '}
+          <Link
+            href="https://www.github.com/matheuxito"
+            underline="hover"
+            color="inherit"
+            target="_blank"
           >
             Matheuxito
           </Link>{' '}
