662d6009ec0057354e21c6b45d04b53ed181ff2d
matheuxito <matheus.souza28042001@gmail.com>
2023-11-07 22:52:28 -0300

    chore: update pcard component

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { RxCode } from 'react-icons/rx';
 import { FaEye } from 'react-icons/fa';
 
 interface ProjectCardProps {
@@ -12,42 +11,33 @@ interface ProjectCardProps {
   projectName: string;
   description: string;
   projectPath: string;
-  codePath: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
   image,
   projectName,
   description,
-  projectPath,
-  codePath
+  projectPath
 }) => {
-  const redirectToCode = () => {
-    window.location.href = codePath;
-  };
-
   const redirectToProject = () => {
     window.location.href = projectPath;
   };
 
   return (
-    <Card sx={{ maxWidth: 600 }}>
+    <Card>
       <CardMedia component="img" alt={projectName} height="140" image={image} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {projectName}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
+        <Typography variant="body2">{description}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={redirectToProject}>
-          View Project <FaEye className="ml-1 text-xl" />
-        </Button>
-        <Button size="small" onClick={redirectToCode}>
-          View Code <RxCode className="ml-1 text-xl" />
-        </Button>
+        <div className="flex justify-evenly w-full">
+          <Button size="small" variant="outlined" onClick={redirectToProject}>
+            View Project <FaEye className="ml-1 text-xl" />
+          </Button>
+        </div>
       </CardActions>
     </Card>
   );
