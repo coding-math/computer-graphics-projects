9491a8864232e4f643a28c6fb86d9a6f64374ace
matheuxito <matheus.souza28042001@gmail.com>
2023-11-12 18:34:36 -0300

    feat: initial setup to the final project

--- src/pages/ProjectsPage/index.tsx
+++ src/pages/ProjectsPage/index.tsx
@@ -1,8 +1,9 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { Footer, ProjectCard } from '../../components';
-import clownImage from '../../assets/images/clown.png';
-import windmillImage from '../../assets/images/windmill.jpg';
+import ClownImage from '../../assets/images/clown.png';
+import WindmillImage from '../../assets/images/windmill.jpg';
+import AirplaneImage from '../../assets/images/airplane.png';
 
 const ProjectsPage = () => {
   return (
@@ -14,7 +15,7 @@ const ProjectsPage = () => {
         <Grid container spacing={2} columns={12}>
           <Grid item xs={12} sm={5} md={4}>
             <ProjectCard
-              image={windmillImage}
+              image={WindmillImage}
               projectName="Windmill"
               description="Experience dynamic rotations within a canvas using keyboard and mouse commands."
               projectPath="/windmill"
@@ -22,10 +23,18 @@ const ProjectsPage = () => {
           </Grid>
           <Grid item xs={12} sm={5} md={4}>
             <ProjectCard
-              image={clownImage}
+              image={ClownImage}
               projectName="Clown"
               description="3D clown model with dynamic movements and animations."
               projectPath="/clown"
+            />
+          </Grid>
+          <Grid item xs={12} sm={5} md={4}>
+            <ProjectCard
+              image={AirplaneImage}
+              projectName="SkyRings"
+              description="SkyRings is my final project, a 3D aviation game crafted using the computer graphics concepts learned throughout the semester."
+              projectPath="/skyrings"
             />
           </Grid>
         </Grid>
