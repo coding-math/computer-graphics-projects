fcfba0051a5afd0317e075680521101cefcdc25e
matheuxito <matheus.souza28042001@gmail.com>
2023-11-13 19:48:59 -0300

    fix: update responsive design

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -24,16 +24,21 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   };
 
   return (
-    <Card>
+    <Card className="flex flex-col min-h-[300px] sm:min-h-[450px] md:min-h-[500px] lg:min-h-[560px]">
       <CardMedia component="img" alt={projectName} height="140" image={image} />
-      <CardContent>
+      <CardContent className="flex-grow">
         <Typography gutterBottom variant="h5" component="div">
           {projectName}
         </Typography>
-        <Typography variant="body2">{description}</Typography>
+        <Typography
+          variant="body2"
+          className="lg:max-h-[60px] lg:overflow-y-auto"
+        >
+          {description}
+        </Typography>
       </CardContent>
       <CardActions>
-        <div className="flex justify-evenly w-full">
+        <div className="flex justify-center w-full">
           <Button size="small" variant="outlined" onClick={redirectToProject}>
             View Project <FaEye className="ml-1 text-xl" />
           </Button>
