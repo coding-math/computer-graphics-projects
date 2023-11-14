7cf7871b971774036ef868f412117cb93b93638c
matheuxito <matheus.souza28042001@gmail.com>
2023-11-14 20:16:58 -0300

    feat: add support for .hdr and .glb files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,7 +2,12 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg';
+declare module '*.hdr';
 declare module '*.gltf' {
+  const value: string;
+  export default value;
+}
+declare module '*.glb' {
   const value: string;
   export default value;
 }
