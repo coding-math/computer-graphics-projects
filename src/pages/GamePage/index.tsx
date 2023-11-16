4d3e598c9dbf012d453674f269470302f91a6557
matheuxito <matheus.souza28042001@gmail.com>
2023-11-16 14:17:00 -0300

    refactor: remove progress bar

--- src/pages/GamePage/index.tsx
+++ src/pages/GamePage/index.tsx
@@ -85,18 +85,6 @@ const GamePage = () => {
               </Suspense>
             </Canvas>
           </div>
-          <div
-            id="progress-bar-container"
-            className="hidden absolute left-0 top-0 w-full aspect-video rounded-t-lg flex-col justify-center items-center bg-black cursor-wait"
-          >
-            <div className="text-white text-lg">Loading...</div>
-            <progress
-              id="progress-bar"
-              className="w-2/5 h-2 mt-2"
-              value="0"
-              max="100"
-            />
-          </div>
           <div className="p-4">
             <p className="font-semibold text-xl mb-3">Controls</p>
             <ul className="list-disc px-4">
