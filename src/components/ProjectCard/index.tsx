527b65119211863c17e88d61c6e6e56f4f908b34
matheuxito <matheus.souza28042001@gmail.com>
2023-11-19 19:31:15 -0300

    feat: improve accessibility

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -19,44 +20,53 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   projectTags
 }) => {
   return (
-    <a href={projectPath} className="no-underline">
-      <Card className="flex flex-col cursor-pointer group hover:bg-gray-200">
-        <CardMedia
-          component="img"
-          alt={projectName}
-          height="140"
-          image={image}
-        />
-        <CardContent className="flex-grow md:min-h-[130px]">
-          <Typography gutterBottom variant="h5" component="div">
-            {projectName}
-          </Typography>
-          <Typography variant="body2" className="max-h-[55px] overflow-y-auto">
-            {description}
-          </Typography>
-        </CardContent>
-        {projectTags && (
-          <div className="mx-auto my-2 flex justify-evenly items-center gap-2">
-            {projectTags.map((tag, index) => (
-              <span
-                key={index}
-                className={`${
-                  tag === 'Easy'
-                    ? 'bg-green-300 group-hover:bg-green-400'
-                    : tag === 'Medium'
-                    ? 'bg-yellow-300 group-hover:bg-yellow-400'
-                    : tag === 'Hard'
-                    ? 'bg-red-300 group-hover:bg-red-400'
-                    : 'bg-gray-300 group-hover:bg-gray-400'
-                } rounded-full px-3 py-1 text-sm font-semibold`}
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-        )}
-      </Card>
-    </a>
+    <>
+      <a
+        href={projectPath}
+        aria-label={`Link to ${projectName} project page, description: ${description}`}
+        className="no-underline"
+      >
+        <Card className="flex flex-col cursor-pointer group hover:bg-gray-200">
+          <CardMedia
+            component="img"
+            alt={projectName}
+            height="140"
+            image={image}
+          />
+          <CardContent className="flex-grow md:min-h-[130px]">
+            <Typography gutterBottom variant="h5" component="div">
+              {projectName}
+            </Typography>
+            <Typography
+              variant="body2"
+              className="min-h-[60px] max-h-[60px] overflow-y-auto"
+            >
+              {description}
+            </Typography>
+          </CardContent>
+          {projectTags && (
+            <div className="mx-auto my-2 flex justify-evenly items-center gap-2">
+              {projectTags.map((tag, index) => (
+                <span
+                  key={index}
+                  className={`${
+                    tag === 'Easy'
+                      ? 'bg-green-300 group-hover:bg-green-400'
+                      : tag === 'Medium'
+                      ? 'bg-yellow-300 group-hover:bg-yellow-400'
+                      : tag === 'Hard'
+                      ? 'bg-red-300 group-hover:bg-red-400'
+                      : 'bg-gray-300 group-hover:bg-gray-400'
+                  } rounded-full px-3 py-1 text-sm font-semibold`}
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          )}
+        </Card>
+      </a>
+    </>
   );
 };
 
