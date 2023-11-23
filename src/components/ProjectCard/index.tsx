a31463bd05cf36999faa897679851fd7855ad4ed
matheuxito <matheus.souza28042001@gmail.com>
2023-11-23 15:39:30 -0300

    feat: add skeleton loader to image

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
 interface ProjectCardProps {
-  image: string;
+  imageUrl: string;
   projectName: string;
   description: string;
   projectPath: string;
@@ -13,12 +13,21 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
-  image,
+  imageUrl,
   projectName,
   description,
   projectPath,
   projectTags
 }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const image = new Image();
+    // image.onload = () => setIsLoading(false);
+    image.src = imageUrl;
+  }, [imageUrl]);
+
   return (
     <>
       <a
@@ -27,12 +36,27 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
         className="no-underline"
       >
         <Card className="flex flex-col cursor-pointer group hover:bg-gray-200">
-          <CardMedia
-            component="img"
-            alt={projectName}
-            height="140"
-            image={image}
-          />
+          {!isLoading ? (
+            <CardMedia
+              component="img"
+              alt={projectName}
+              height="140"
+              image={imageUrl}
+            />
+          ) : (
+            <div className="animate-pulse flex items-center justify-center aspect-square bg-gray-700">
+              <svg
+                className="w-10 h-10 text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 16 20"
+              >
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+              </svg>
+            </div>
+          )}
           <CardContent className="flex-grow md:min-h-[130px]">
             <Typography gutterBottom variant="h5" component="div">
               {projectName}
